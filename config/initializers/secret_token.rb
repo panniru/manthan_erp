663535821eefb894689bcbf2959b26269b94eaa2
Srikanth <sri.panniru08@gmail.com>
2014-05-15 18:03:00 +0530

    Plain Rails App-first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ManthanErp::Application.config.secret_key_base = '5ec452802aa3e6956c0281175d93fd3a803b90712bc982a3803af24ac832d256db53d5e07be9ae3f191f34826df2a80ab4f697895f4055bd499e83f497b25364'
