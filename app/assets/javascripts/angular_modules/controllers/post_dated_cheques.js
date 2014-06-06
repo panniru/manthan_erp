47cf612a2ceeeb3ff32fb0cee111f633763722e9
chandrashekargupta <your.chandu.gupta16@gmail.com>
2014-06-06 15:43:23 +0530

    modal code in partial files

--- app/assets/javascripts/angular_modules/controllers/post_dated_cheques.js
+++ app/assets/javascripts/angular_modules/controllers/post_dated_cheques.js
@@ -7,28 +7,13 @@
 		$scope.post_dated_cheques.push({"date":""});
             };
 	$scope.showModal = function(){
-	    alert('')
 	    $("#myModal").modal()
 	}
 	    $scope.post_dated_cheque = {"date":"", "amount_per":""}
 	    $scope.save = function(){
 		$scope.post_dated_cheques.push( $scope.post_dated_cheque);
             };
-    
-	    
-	$scope.validation = function(){
-	    var lnt = parseInt($scope.post_dated_cheques.length)
-	    var sum = 0
-	    for(var i =0;i<lnt;i++){
-		alert($scope.post_dated_cheques.amount_per)
-		sum += parseInt($scope.post_dated_cheques.amount_per) ;
-	    };
-	    if (sum != 100)
-		alert ('Percent not equal to 100');
-	    else
-		alert ('save?');
-		
-	}
-	}  
-     }]);
- })(angular, myApp);
+    	}  
+    }]);
+})(angular, myApp);
+>>>>>>> Stashed changes
--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,34 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-ActiveRecord::Schema.define(version: 20140529104332) do
+ActiveRecord::Schema.define(version: 20140529105755) do
 
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "delayed_jobs", force: true do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+
+  create_table "fee_grade_buckets", force: true do |t|
+    t.string   "bucket_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
 
   create_table "fee_types", force: true do |t|
     t.string   "fee_type"
