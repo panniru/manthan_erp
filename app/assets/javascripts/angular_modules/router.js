cd435fcde1ac448d12e88947625b3ab3d49f163d
Srikanth <sri.panniru08@gmail.com>
2014-06-10 14:00:34 +0530

    production minifest change for angular router

--- app/assets/javascripts/angular_modules/router.js
+++ app/assets/javascripts/angular_modules/router.js
@@ -1,18 +1,18 @@
 (function(angular, app) {
     "use strict";
-    app.config(function($routeProvider, $locationProvider) {
+    app.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
         $routeProvider
             .when('/annual_payment', {
-                templateUrl: 'annual_fee_payment.html'
+                "templateUrl": 'annual_fee_payment.html'
             })
             .when('/term_wise_payment', {
-                templateUrl: 'term_fee_payment.html'
+                "templateUrl": 'term_fee_payment.html'
             })
             .when('/monthly_payment', {
-                templateUrl: 'month_fee_payment.html'
+                "templateUrl": 'month_fee_payment.html'
             })
        // $locationProvider.html5Mode(true); 
-    });
+    }]);
 })(angular, myApp);
 
 
