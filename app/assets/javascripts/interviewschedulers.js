af922ec88d70ee0e4615ea72bb784d4a3fa293b3
Srikanth <sri.panniru08@gmail.com>
2014-06-20 18:21:12 +0530

    js fix in interviwe schedular

--- app/assets/javascripts/interviewschedulers.js
+++ app/assets/javascripts/interviewschedulers.js
@@ -1,8 +1,4 @@
 
-    // page is now ready, initialize the calendar...
-
-    $('#calendar').fullCalendar({
-        interviewschedulers: '/interviewschedulers.json'        // put your options and callbacks here
-    })
-
+$('#calendar').fullCalendar({
+    interviewschedulers: '/interviewschedulers.json'        // put your options and callbacks here
 });
