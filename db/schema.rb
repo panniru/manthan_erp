69da2b4bcdb632f79736adb17b25c6c9fabf0435
Swamy <swamy@ostryalabs.com>
2014-06-30 19:21:25 +0530

    modified teachingplan form,index

--- db/schema.rb
+++ db/schema.rb
@@ -241,6 +241,12 @@ ActiveRecord::Schema.define(version: 20140627092858) do
     t.datetime "updated_at"
   end
 
+  create_table "grades", force: true do |t|
+    t.string   "grade"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "interviewschedulers", force: true do |t|
     t.string   "name"
     t.string   "description"
@@ -313,7 +319,7 @@ ActiveRecord::Schema.define(version: 20140627092858) do
     t.string   "academic_year"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "term_definition_id"
+    t.integer  "payment_detail_id"
     t.string   "status"
   end
 
@@ -389,6 +395,12 @@ ActiveRecord::Schema.define(version: 20140627092858) do
     t.string   "grade_master_id"
   end
 
+  create_table "sections", force: true do |t|
+    t.string   "section"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "staffs", force: true do |t|
     t.string   "staff_name"
     t.string   "staff_exp"
@@ -420,6 +432,12 @@ ActiveRecord::Schema.define(version: 20140627092858) do
     t.datetime "updated_at"
   end
 
+  create_table "subjects", force: true do |t|
+    t.string   "subject"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "teacher_grade_mappings", force: true do |t|
     t.string   "grade_master_id"
     t.string   "section_master_id"
@@ -445,15 +463,20 @@ ActiveRecord::Schema.define(version: 20140627092858) do
   end
 
   create_table "teaching_plans", force: true do |t|
+
     t.string   "plan_month"
     t.integer  "user_id"
     t.string   "academic_year"
+
+
     t.string   "grade_master_id"
     t.string   "section_master_id"
     t.string   "plan"
     t.date     "teaching_date"
+
     t.datetime "created_at"
     t.datetime "updated_at"
+
   end
 
   create_table "term_definitions", force: true do |t|
@@ -480,7 +503,6 @@ ActiveRecord::Schema.define(version: 20140627092858) do
 
   create_table "time_tables", force: true do |t|
     t.string   "academic_year"
-    t.string   "staffid"
     t.string   "mon_sub"
     t.string   "tue_sub"
     t.string   "wed_sub"
