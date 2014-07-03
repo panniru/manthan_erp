0d88e4f4c662fd0a255d554eff847843e44dd4e6
Muralee Mohan <muraleem3@gmail.com>
2014-07-03 19:32:30 +0530

    multi section select in teacher grade mapping

--- app/controllers/teacher_grade_mappings_controller.rb
+++ app/controllers/teacher_grade_mappings_controller.rb
@@ -11,21 +11,27 @@ class TeacherGradeMappingsController < ApplicationController
   end
 
   def savemappings 
-    p params
-    p "$$$$$$$$$$"
-    params[:_mappings].each do |t| 
-      if t["id"].present?
-        p t["id"]
-        temp = TeacherGradeMapping.find(t["id"])
-        temp.grade_master_id = t["grade_master_id"]
-        temp.section_master_id = t["section_master_id"]
-        temp.subject_master_id = t["subject_master_id"]       
-        temp.save
-      else
-        @timetable = TeacherGradeMapping.new(t)
-        @timetable.save
+    respond_to do |format|
+      format.json do
+        p params
+        p "$$$$$$$$$$"
+        params[:_mappings].each do |t| 
+          if t["id"].present?
+            p t["id"]
+            temp = TeacherGradeMapping.find(t["id"])
+            temp.grade_master_id = t["grade_master_id"].to_i
+            temp.section_master_id = t["section_master_id"].to_i
+            temp.subject_master_id = t["subject_master_id"].to_i       
+            temp.save
+          else
+            @timetable = TeacherGradeMapping.new(t)
+            @timetable.save
+          end
+        end
+        render :json => true
       end
     end
+
   end
 
   def getmappings 
@@ -36,7 +42,22 @@ class TeacherGradeMappingsController < ApplicationController
     p teachersgrademappings
     p "!!!!!!!!!!!!!!!"
     teachersgrademappings = teachersgrademappings.all.map do |mapping|
-      {id: mapping.id,  grade_master_id: mapping.grade_master_id, grade_name: mapping.grade_master.grade_name, section_master_id: mapping. section_master_id, section_name: mapping.section_master.section, subject_master_id: mapping.subject_master_id ,subject_name: mapping.subject_master.subject }
+      if  (mapping.subject_master_id.present?)
+        sub_name = mapping.subject_master.subject
+      else
+        sub_name = nil
+      end
+      if  (mapping.grade_master_id.present?)
+        grad_name = mapping.grade_master.grade_name
+      else
+        grad_name = nil
+      end
+      if  (mapping.section_master_id.present?)
+        sec_name = mapping.section_master.section
+      else
+        sec_name = nil
+      end
+      {id: mapping.id,  grade_master_id: mapping.grade_master_id.to_i, grade_name: grad_name, section_master_id: mapping.section_master_id.to_i, section_name: sec_name, subject_master_id: mapping.subject_master_id.to_i ,subject_name: sub_name }
     end    
     render :json => teachersgrademappings
     p teachersgrademappings
