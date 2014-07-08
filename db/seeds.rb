ba9e739785a7fdd33db9ec3edc59dd4a17ed0111
Muralee Mohan <muraleem3@gmail.com>
2014-07-08 18:02:36 +0530

    seed.rb error fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -118,7 +118,7 @@ def seed_students
   student = StudentMaster.first
   unless student.present?
     StudentMaster.create(:name => "Sankl", :dob => "17-06-1989", :joining_date => DateTime.now, :academic_year => "#{DateTime.now.year}-#{DateTime.now.year+1}", :parent_id => Parent.first.id, :grade_master_id => GradeMaster.first.id, :section_master_id => SectionMaster.first.id)
-    StudentMaster.create(:name => "Srikanth", :dob => "17-06-1989", :joining_date => DateTime.now, :academic_year => "#{DateTime.now.year}-#{DateTime.now.year+1}", :parent_id => Parent.first.id, :grade_master_id => GradeMaster.last.id, :section_master_id => SectionMaster.last.id))
+    StudentMaster.create(:name => "Srikanth", :dob => "17-06-1989", :joining_date => DateTime.now, :academic_year => "#{DateTime.now.year}-#{DateTime.now.year+1}", :parent_id => Parent.first.id, :grade_master_id => GradeMaster.last.id, :section_master_id => SectionMaster.last.id)
   end
 end
 
