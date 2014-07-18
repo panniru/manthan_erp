14cb8fe03ad853f2776008aad9fe52daa34b2598
Muralee Mohan <muraleem3@gmail.com>
2014-07-18 19:26:35 +0530

    grades_sections_mapping

--- app/controllers/class_teacher_mappings_controller.rb
+++ app/controllers/class_teacher_mappings_controller.rb
@@ -3,15 +3,12 @@ class ClassTeacherMappingsController < ApplicationController
   def get_class_teacher_mappings
     respond_to do |format|
       format.json do
-        p my_Grade = params[:my_Grade].to_i
+        my_Grade = params[:my_Grade].to_i
         classteachermappings = ClassTeacherMapping.where('grade_master_id = '+"#{my_Grade}") 
         classteachermappings = classteachermappings.all.map do |mapping|
           {id: mapping.id, grade_master_id: mapping.grade_master_id, grade_name: mapping.grade_master.grade_name, section_master_id: mapping.section_master_id,  section_name: mapping.section_master.section_name, faculty_master_id: mapping.faculty_master_id, faculty_name: mapping.faculty_master.faculty_name}          
-        end
-        p classteachermappings.length
-        render :json =>classteachermappings
-        p "@@@@@@@@@@@@@@@@@@@@=>"
-        p classteachermappings
+        end        
+        render :json =>classteachermappings        
       end
     end
   end
@@ -20,9 +17,9 @@ class ClassTeacherMappingsController < ApplicationController
   def get_grade_wise_mappings    
     respond_to do |format|
       format.json do
-        p my_Grade = params[:my_Grade]
+        my_Grade = params[:my_Grade]
            
-            teachersgrademappings = TeacherGradeMapping.where('grade_master_id = '+"#{my_Grade}")        
+        teachersgrademappings = TeacherGradeMapping.where('grade_master_id = '+"#{my_Grade}")        
       
         teachersgrademappings = teachersgrademappings.all.map do |mapping|
           if  (mapping.subject_master_id.present?)
@@ -42,10 +39,8 @@ class ClassTeacherMappingsController < ApplicationController
           end
           {id: "",  grade_master_id: mapping.grade_master_id.to_i, grade_name: grad_name, section_master_id: mapping.section_master_id.to_i, section_name: sec_name, grade_section: mapping.grade_master_id.to_s+"-"+mapping.section_master_id.to_s, faculty_master_id:"", faculty_name:""}
           
-        end       
-        p teachersgrademappings        
-        t = teachersgrademappings.uniq { |h| h[:grade_section] }      
-        p "^^^^^^^^^^^^^^^^^>"
+        end          
+        t = teachersgrademappings.uniq { |h| h[:grade_section] }        
         render :json =>t
        
       end
@@ -61,31 +56,23 @@ class ClassTeacherMappingsController < ApplicationController
         
         teachersgrademappings = teachersgrademappings.all.map do |mapping|
           {id: mapping.id, faculty_master_id: mapping.faculty_master_id, faculty_name: mapping.faculty_master.faculty_name}
-          end
-        p teachersgrademappings        
-        t = teachersgrademappings.uniq { |h| h[:faculty_master_id] }  
-        p "====================>"
-        p t
+          end               
+        t = teachersgrademappings.uniq { |h| h[:faculty_master_id] }        
         render :json =>t        
       end
     end
 
     def save_mappings
       respond_to do |format|
-        format.json do
-          p params
-          p "%%%%%%%%%%%%%%>"
+        format.json do          
           params[:mappings].each do |t| 
-            if t["id"].present?
-              p "JJJJJJJJ"
-              p t["id"]
+            if t["id"].present?              
               temp = ClassTeacherMapping.find(t["id"])
               temp.grade_master_id = t["grade_master_id"]
               temp.section_master_id = t["section_master_id"]
               temp.faculty_master_id = t["faculty_master_id"]    
               temp.save
-            else
-              p "CCC"
+            else              
               @mapping = ClassTeacherMapping.new(t)
               @mapping.save
             end
@@ -99,9 +86,7 @@ class ClassTeacherMappingsController < ApplicationController
 
   def deletemappings
     respond_to do |format|
-      format.json do 
-        p params[:_delete_mapping_id]
-        p "=======>--------->>>>>>>>"
+      format.json do         
         if params[:_delete_mapping_id].present?
           ClassTeacherMapping.find(params[:_delete_mapping_id]).destroy          end
         render :json => true
@@ -111,10 +96,7 @@ class ClassTeacherMappingsController < ApplicationController
 
   def check_for_teacher_mapping
     respond_to do |format|
-      format.json do 
-        p params
-        p params[:_faculty_ids]
-        p "=======>--+++++++-->>>>>>>>"
+      format.json do         
         faculty_ids = ClassTeacherMapping.all
         faculty_ids = faculty_ids.all.map do |mapping|
           {faculty_id: mapping.faculty_master_id}
@@ -123,11 +105,4 @@ class ClassTeacherMappingsController < ApplicationController
       end
     end    
   end
-
-
- 
-        
-
-
-
 end
--- app/controllers/grades_sections_mappings_controller.rb
+++ app/controllers/grades_sections_mappings_controller.rb
@@ -1,2 +1,27 @@
 class GradesSectionsMappingsController < ApplicationController
+
+  def save_sections_mappings
+    respond_to do |format|
+      format.json do
+        p params
+        p "===========>"
+        mappings = params[:mappings]
+        p mappings
+        p "===========>"
+
+        params[:mappings].each do |t| 
+          if t["id"].present?           
+            temp=SectionMaster.find(t["id"])        
+            temp.section_name = t['section_name']
+            temp.grade_master_id = t['grade_master_id']
+            temp.update
+          else
+            @sectionmaster=SectionMaster.new(t)
+            @sectionmaster.save
+          end
+        end       
+      end
+    end
+  end
+
 end
