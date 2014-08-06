e5101f75dab2344f71b8737a615396dd31e6a0b2
Srikanth <sri.panniru08@gmail.com>
2014-08-06 23:18:14 +0530

    blunder in user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,16 +1,8 @@
 class UserMailer < ActionMailer::Base
   default from: "from@example.com"
-  if current_user.admin?
-    def welcome(mails, subject, text)
-      @subject = subject
-      @text = text
-      mail(to: mails , subject: @subject )
-    end
-  elsif current_user.parent?
-    def welcome( subject, text)
-      @subject = subject
-      @text = text
-      mail(to: 'navya@ostryalabs.com' , subject: @subject )
-    end
+  def welcome(mails, subject, text)
+    @subject = subject
+    @text = text
+    mail(to: mails , subject: @subject )
   end
 end
