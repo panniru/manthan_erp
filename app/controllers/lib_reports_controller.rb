4ffadd4456618dafd436266beaec5e67539c17d2
umagaini <umagaini2@gmail.com>
2014-08-21 18:59:37 +0530

    reports

--- app/controllers/lib_reports_controller.rb
+++ app/controllers/lib_reports_controller.rb
@@ -1,9 +1,19 @@
 class LibReportsController < ApplicationController
+ 
   def new
     @lib_report = LibReport.new
   end
   def index
-    @books = Book.all
-   
+    # @books = Book.all
+     @books = Book.all.sort_by &:updated_at
+    
   end
+  def Leastused
+    #@issuings = Issuing.all
+    # @issuings = Issuing.group("book").count
+    @issuings = Issuing.group("book").count
+    
+    
+  end
+ 
 end
