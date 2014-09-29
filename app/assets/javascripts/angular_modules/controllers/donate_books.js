7abaee7483ecdeeb48b2263caf45a41496a51f35
Muralee Mohan <muraleem3@gmail.com>
2014-09-29 09:51:24 +0530

    donation of books

--- app/assets/javascripts/angular_modules/controllers/donate_books.js
+++ app/assets/javascripts/angular_modules/controllers/donate_books.js
@@ -3,6 +3,22 @@
     app.controller("DonateBooksController",["$scope","resourceService", "donateBooksService", function($scope, resourceService, donateBooksService) {         
         $scope.donate_books = resourceService.DonateBook.query();  
 
-    }]);  
     
+    $scope.editDonateBook = function(donate_book){
+        //alert(JSON.stringify(donate_book));
+        $scope.donate_book
+        $('#myEditModal').modal('show');
+
+    };
+        $scope.updateDonateBook = function(donate_book){
+            //alert(JSON.stringify(donate_book));
+            $('#myEditModal').modal('hide');
+            donateBooksService.updateDonateBook($scope.donate_book)
+                .then(function(result){
+                });
+        };
+
+
+
+    }]); 
 })(angular, myApp);
--- app/assets/javascripts/angular_modules/services/donate_books_service.js
+++ app/assets/javascripts/angular_modules/services/donate_books_service.js
@@ -1,8 +1,13 @@
 (function(angular, app) {
     "use strict";
     app.service("donateBooksService",["$http", function($http) {
+        var updateDonateBook = function(donate_book){            
+            var url = "/donate_books/update_donate_book.json"
+            return $http.post(url, {donate_book: donate_book});    
+        };
              
         return{
+            updateDonateBook : updateDonateBook,
         }       
     }]);
 })(angular, myApp);
