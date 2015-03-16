6ceb4e775a45f1b7e24cf66a5eaafb9dcc800502
MURALEE <muraleem3@gmail.com>
2015-03-16 11:44:31 +0530

    assessment results

--- app/assets/javascripts/angular_modules/services/activity_teachers_map_service.js
+++ app/assets/javascripts/angular_modules/services/activity_teachers_map_service.js
@@ -1,12 +1,12 @@
 (function(angular, app) {
     "use strict";
     app.service("activityTeachersMapService",["$http", function($http) {
-
+        
         var checkActivityTeachersMapping = function(myActivity){          
             var url = "/activity_teachers_mappings/check_activity_teachers_mapping.json"             
             return $http.get(url,{params:{myActivity: myActivity}});  
         }; 
-
+        
         var saveMappings = function(mappings){
             var url = "/activity_teachers_mappings/savemappings.json"
             return $http.post(url, {mappings: mappings});            
@@ -14,14 +14,14 @@
         
         var getMappings = function(myActivity){
             var url = "/activity_teachers_mappings/getmappings.json"
-            return $http.post(url, {my_Activity: myActivity}); 
+            return $http.get(url, {params:{my_Activity: myActivity}}); 
         }; 
-
+        
         var deleteMapping = function(delete_mapping_id){
             var url = "/activity_teachers_mappings/deletemapping.json"
             return $http.post(url, {delete_mapping_id: delete_mapping_id}); 
         };
-       
+        
         return {
             checkActivityTeachersMapping : checkActivityTeachersMapping,
             saveMappings : saveMappings,
@@ -29,5 +29,5 @@
             deleteMapping : deleteMapping
         };        
     }]);   
-
+    
 })(angular, myApp);
